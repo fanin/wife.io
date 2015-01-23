7577ad0fe38051959a5d19ee5fa77a973a48438a
Kenny Chen <knych.csy@gmail.com>
2015-01-23 17:07:44 +0800

    build-system: concat ui-kits js & css

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,106 @@
 var gulp = require('gulp'),
+    browserify = require('browserify'),
     uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss'),
     concat = require('gulp-concat'),
     jsonmin = require('gulp-jsonmin'),
+    source = require('vinyl-source-stream'),
     del = require('del');
 
 var JQUERY_VERSION = '1.11.1',
     FONTAWESOME_VERSION = '4.2.0';
 
+var OUTPATH = 'build';
+
 gulp.task('server', function() {
     gulp.src(['*.js', '!gulpfile.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(OUTPATH));
     gulp.src('server/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('build/server'));
+        .pipe(gulp.dest(OUTPATH + '/server'));
 });
 
 gulp.task('lib', function() {
+    /* Create foundation framework bundle */
     gulp.src('lib/framework/foundation/**/*.js')
         .pipe(concat('foundation-bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('build/lib/framework/foundation/'));
-    gulp.src([
-            'lib/framework/ui/**/*',
-            '!lib/framework/ui/**/*.js',
-            '!lib/framework/ui/**/*.css'
-        ])
-        .pipe(gulp.dest('build/lib/framework/ui'));
+        .pipe(gulp.dest(OUTPATH + '/lib/framework/foundation/'));
+
+    /* Create UIKit js bundle */
     gulp.src('lib/framework/ui/**/*.js')
+        .pipe(concat('ui-kits-bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('build/lib/framework/ui'));
+        .pipe(gulp.dest(OUTPATH + '/lib/framework/ui'));
+
+    /* Create UIKit css bundle */
     gulp.src('lib/framework/ui/**/*.css')
+        .pipe(concat('ui-kits-bundle.css'))
         .pipe(uglifycss())
-        .pipe(gulp.dest('build/lib/framework/ui'));
+        .pipe(gulp.dest(OUTPATH + '/lib/framework/ui'));
+
+    /* Copy UIKit images */
+    gulp.src([
+        'lib/framework/ui/**/*',
+        '!lib/framework/ui/**/js',
+        '!lib/framework/ui/**/css',
+        '!lib/framework/ui/**/*.js',
+        '!lib/framework/ui/**/*.css'
+    ])
+    .pipe(gulp.dest(OUTPATH + '/lib/framework/ui'));
+
+    /* Copy 3rd party libraries */
     gulp.src('lib/jquery/jquery-' + JQUERY_VERSION + '.min.js')
-        .pipe(gulp.dest('build/lib/jquery'));
+        .pipe(gulp.dest(OUTPATH + '/lib/jquery'));
     gulp.src('lib/jquery/plugins/**/*')
-        .pipe(gulp.dest('build/lib/jquery/plugins'));
+        .pipe(gulp.dest(OUTPATH + '/lib/jquery/plugins'));
     gulp.src('lib/jquery/ui/' + JQUERY_VERSION + '/**/*')
-        .pipe(gulp.dest('build/lib/jquery/ui/' + JQUERY_VERSION));
+        .pipe(gulp.dest(OUTPATH + '/lib/jquery/ui/' + JQUERY_VERSION));
     gulp.src('lib/font-awesome/' + FONTAWESOME_VERSION + '/**/*')
-        .pipe(gulp.dest('build/lib/font-awesome/' + FONTAWESOME_VERSION));
+        .pipe(gulp.dest(OUTPATH + '/lib/font-awesome/' + FONTAWESOME_VERSION));
+});
+
+gulp.task('ui-kits', function() {
+    return browserify({
+        entries: ['./lib/framework/ui/ui-kits.js'],
+        standalone: 'Page',
+        debug: true
+    })
+    .bundle()
+    .pipe(source('ui-kits-bundle.js'))
+    .pipe(gulp.dest(OUTPATH + '/lib/framework/ui/'))
 });
 
 gulp.task('app', function() {
     gulp.src('apps/**/*')
-        .pipe(gulp.dest('build/apps'));
+        .pipe(gulp.dest(OUTPATH + '/apps'));
 });
 
 gulp.task('api', function() {
     gulp.src('api/*.json')
         .pipe(jsonmin())
-        .pipe(gulp.dest('build/api'));
+        .pipe(gulp.dest(OUTPATH + '/api'));
 });
 
 gulp.task('resource', function() {
     gulp.src('resources/**/*')
-        .pipe(gulp.dest('build/resources'));
+        .pipe(gulp.dest(OUTPATH + '/resources'));
 });
 
 gulp.task('config', function() {
     gulp.src(['package.json', 'settings.json'])
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(OUTPATH));
 });
 
 gulp.task('clean', function(cb) {
-    del('build', cb);
+    del(OUTPATH, cb);
 });
 
 gulp.task('default', [
     'server',
     'lib',
+    //'ui-kits',
     'app',
     'api',
     'resource',
